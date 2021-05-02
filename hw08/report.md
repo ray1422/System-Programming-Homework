708b97bc68e73a5720dd3f3595fd7ad04d650bfe
Ray1422 <ray20020809@gmail.com>
2021-05-02 13:07:56 +0800

    add hw08

--- hw08/report.md
+++ hw08/report.md
@@ -2,10 +2,29 @@
 
 409410005 鍾天睿
 
-## 1. Nice
+## 1. capabilities
 
-![Screenshot from 2021-04-29 15-43-54](/home/pc341/Pictures/Screenshot from 2021-04-29 15-43-54.png)
+### CAP_CHOWN
+
+允許改變檔案的 UID 跟 GID
+
+### CAP_IPC_LOCK
+
+允許鎖定記憶體
+
+### CAP_SETUID
+
+允許 setuid
 
 
 
 ## 2. 量測 Nice
+
+```
+❯ ./a.out
+p*: 183903292
+c*: 573114164
+❯ = 573114164 / 183903292.
+3.11639
+```
+
--- hw08/test.c
+++ hw08/test.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#define __USE_GNU
 #include <unistd.h>
+#include <sched.h>
+#include <pthread.h>
+
 sig_atomic_t alrm = 1;
-int main() {
-	pid_t pid = fork();
-	if (pid == 0) { 
-		
-	} else {
-		nice(5);
-	}
-	int l = 0;
-	// alarm(1);
-	while(alrm) {
-		l++;
-	}
-	if (pid == 0) {
-		printf("c*: %d\n", l);
-	} else {
-		printf("p*: %d\n", l);
-	}
-	
+
+void alrm_handler() {
+    alrm = 0;
 }
+int main() {
+    pid_t pid = fork();
+    if (pid == 0) { 
+    } else {
+        nice(-5);
+    }
+    cpu_set_t mask;  //CPU核的集合
+    cpu_set_t get;   //获取在集合中的CPU
+    CPU_ZERO(&mask);    //置空
+    CPU_SET(0, &mask);   // 将当前线程和CPU绑定
+    if(sched_setaffinity(0, sizeof(mask), &mask)) {
+        perror("CPU 親和力設定失敗");
+        abort();
+    }
+
+    int l = 0;
+    signal(SIGALRM, alrm_handler);
+    alarm(1);
+    while(alrm) {
+        l++;
+    }
+    if (pid == 0) {
+        printf("c*: %d\n", l);
+    } else {
+        printf("p*: %d\n", l);
+    }
+}
+
